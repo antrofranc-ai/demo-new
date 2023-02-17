1681236f5dbdf2a8ee12492f4e08677495e22fa3
sinth <sinthiyamarian@gmail.com>
2023-02-16 19:00:18 -0500

    Formatted code

--- src/main/java/com/example/demonew/SearchAction.java
+++ src/main/java/com/example/demonew/SearchAction.java
@@ -1,30 +1,18 @@
 package com.example.demonew;
 
 import com.intellij.ide.BrowserUtil;
-import com.intellij.lang.Language;
-import com.intellij.openapi.actionSystem.*;
-import com.intellij.openapi.editor.CaretModel;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.Presentation;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.Optional;
 
 public class SearchAction extends AnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
 
-        /*Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
-        CaretModel caretModel = editor.getCaretModel();
-        String  selectedText = caretModel.getCurrentCaret().getSelectedText();
-        String query = selectedText.replace(' ', '+');*/
-
-        BrowserUtil.browse("https://www.google.com/search?q="  + "Java whats new");
+        BrowserUtil.browse("https://www.google.com/search?q=" + "Java whats new");
     }
-
 
     public void update(@NotNull AnActionEvent e) {
         super.update(e);
